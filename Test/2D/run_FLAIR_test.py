949fae7fb322df3b61262230a8eb64ccbb4a6711
ADITYA RASTOGI <56337920+adityarastogi2k12@users.noreply.github.com>
2023-10-16 18:01:30 +0200

    Update run_FLAIR_test.py

--- Test/2D/run_FLAIR_test.py
+++ Test/2D/run_FLAIR_test.py
@@ -1,18 +1,24 @@
-import os,time
+# Import necessary libraries
+import os
+import time
 import subprocess as sp
 from datetime import datetime
 
-
-
-import os,time
-import subprocess as sp
-from datetime import datetime
-
+# Specify the GPU to use (in this case, GPU 0)
 gpu_id = '0'
-model_dir='../../Weights/2D/'
 
-csv_path ='FLAIR_2D_CSV_PATH.csv'
+# Define the directory where model weights are stored
+model_dir = '../../Weights/2D/'
+
+# Define the path to the CSV file containing data (assuming it contains FLAIR data)
+csv_path = 'FLAIR_2D_CSV_PATH.csv'
+
+# Set the undersampling factor (R)
 R = 2
 
+# Construct the command to run a Python script with arguments
+# This command runs 'test_FLAIR_2D_tf2.py' with specified arguments
 cmd = f"python3 test_FLAIR_2D_tf2.py --R {R} --K 10 --csv_path csv {csv_path} --model_dir={model_dir} "
-sp.call(cmd,shell =True)
+
+# Execute the command in the shell
+sp.call(cmd, shell=True)
